2c8f846424cbfa9b959e8aacb8a9ec7e856d09a0
stephenLYao <750188453@qq.com>
2018-10-26 16:48:52 +0800

    feat

--- src/App.js
+++ src/App.js
@@ -1,28 +1,45 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+export const App = () => {
+  const [value, setValue] = useState('')
+  const [lists, setLists] = useState([])
+  const onChange = (e) => {
+    setValue(e.currentTarget.value)
   }
+  const onSubmit = (e) => {
+    if (e.keyCode === 13) {
+      const newLists = lists.concat({ value: value, isCompleted: false })
+      setLists(newLists)
+      setValue('')
+    }
+  }
+  const onComplete = (e, index) => {
+    const newLists = lists.slice(0)
+    const newList = { value: lists[index].value, isCompleted: !lists[index].isCompleted }
+    newLists.splice(index, 1, newList)
+    setLists(newLists)
+  }
+  const onDelete = (e, index) => {
+    const newLists = lists.slice(0)
+    newLists.splice(index, 1)
+    setLists(newLists)
+  }
+  return (
+    <div className="app">
+      <p>Todo</p>
+      <input type="text" value={value} onChange={(e) => onChange(e)} onKeyUp={(e) => onSubmit(e)} />
+      <ul>
+        { lists.map((list, index) => {
+          return (
+            <li key={index}>
+              <span className={list.isCompleted ? 'isCompleted': ''}>{list.value}</span>
+              <button onClick={(e) => {onComplete(e, index)}}>√</button>
+              <button onClick={(e) => {onDelete(e, index)}}>x</button>
+            </li>
+          ) 
+        })}
+      </ul>
+    </div>
+  )
 }
-
-export default App;
